19e65bc285e957f06206d029afb69098c74d1549
Admin <arab1997>
2021-01-25 10:15:13 +0500

    added Search

--- app/src/main/java/com/gelostech/bloggerappkotlin/MainActivity.kt
+++ app/src/main/java/com/gelostech/bloggerappkotlin/MainActivity.kt
@@ -4,6 +4,7 @@ import android.app.DownloadManager
 import android.app.ProgressDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
@@ -24,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var adapterPost: AdapterPost
     private lateinit var progressDialog: ProgressDialog
 
+    private var isSearch = false
     private val TAG = "MAIN_TAG"
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,12 +43,34 @@ class MainActivity : AppCompatActivity() {
 
         //handle click, load more psots
         loadMoreBtn.setOnClickListener {
-            loadPosts()
+            val query = searchEt.text.toString().trim()
+            if (TextUtils.isEmpty(query)){
+                loadPosts()
+            }
+            else{
+                searchPosts(query)
+            }
         }
+        //search
+        searchBtn.setOnClickListener {
+            nextToken = ""
+            url = ""
 
+            postArrayList = ArrayList()
+            postArrayList.clear()
+            loadMoreBtn.isEnabled = true
+            val query = searchEt.text.toString().trim()
+            if (TextUtils.isEmpty(query)){
+                loadPosts()
+            }
+            else{
+                searchPosts(query)
+            }
+        }
     }
 
     private fun loadPosts() {
+        isSearch = false
         progressDialog.show()
 
         url = when (nextToken) {
@@ -84,6 +108,103 @@ class MainActivity : AppCompatActivity() {
                 } catch (e: Exception) {
                     Toast.makeText(this, "Reached end of the page..", Toast.LENGTH_SHORT).show()
                     Log.d(TAG, "loadPosts: NextPageToken: Reached end of the page..")
+                    nextToken = "end"
+                }
+                //get json array data from json object
+                val jsonArray = jsonObject.getJSONArray("items")
+                // continue getting data untill complated all
+                for (i  in 0 until  jsonArray.length()){
+                    try {
+                        val jsonObject01 = jsonArray.getJSONObject(i)
+
+                        val id = jsonObject01.getString("id")
+                        val title = jsonObject01.getString("title")
+                        val content = jsonObject01.getString("content")
+                        val published = jsonObject01.getString("published")
+                        val updated  = jsonObject01.getString("updated")
+                        val url  = jsonObject01.getString("url")
+                        val selfLink  = jsonObject01.getString("selfLink")
+                        val authorName  = jsonObject01.getJSONObject("author").getString("displayName")
+                        val image  = jsonObject01.getJSONObject("author").getString("displayName")
+
+                        //set data
+                        val modelPost = ModelPost(
+                            "$authorName",
+                            "$content",
+                            "$id",
+                            "$published",
+                            "$selfLink",
+                            "$title",
+                            "$updated",
+                            "$url"
+                        )
+
+                        //add data to list
+                        postArrayList.add(modelPost)
+                    }
+                    catch (e:Exception){
+                        Log.d(TAG, "loadPosts: 1 ${e.message}")
+                        Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+                    }
+                }
+                //setup  adapter
+                adapterPost = AdapterPost(this@MainActivity, postArrayList )
+                //set adapter  to recycler
+                postsRv.adapter = adapterPost
+                progressDialog.dismiss()
+            } catch (e: Exception) {
+                Log.d(TAG, "loadPosts: 2 ${e.message}")
+                Toast.makeText(this, "${e.message}", Toast.LENGTH_SHORT).show()
+            }
+
+        }, { error ->
+            Log.d(TAG, "loadPosts: ${error.message}")
+            Toast.makeText(this, "${error.message}", Toast.LENGTH_SHORT).show()
+        })
+        //add  requset in queue
+        val requestQueue =Volley.newRequestQueue(this)
+        requestQueue.add(stringRequest)
+    }
+
+    private fun searchPosts(query:String){
+        isSearch = true
+        progressDialog.show()
+
+        url = when (nextToken) {
+            "" -> {
+                Log.d(TAG, "searchPosts: NextPageToken is empty, more psots")
+                ("https://www.googleapis.com/blogger/v3/blogs/${Constants.BLOG_ID}/posts/search?q=$query&&key=${Constants.API_KEY}")
+            }
+            "end" -> {
+                Log.d(TAG, "searchPosts: Next page token is end, no more post  i.e loded all posts")
+                Toast.makeText(this, " No more posts", Toast.LENGTH_SHORT).show()
+                progressDialog.dismiss()
+                return
+            }
+            else -> {
+                Log.d(TAG, "searchPosts: Next page Token: $nextToken")
+                ("https://www.googleapis.com/blogger/v3/blogs/${Constants.BLOG_ID}/posts/search?q=$query&pageToken=$nextToken=${Constants.API_KEY}")
+            }
+        }
+        Log.d(TAG, "searchPosts: URL: $url")
+
+        //request data , Method is GET
+        val stringRequest = StringRequest(Request.Method.GET, url, { response ->
+            // we get response, so dissmisdialog first
+            progressDialog.dismiss()
+            Log.d(TAG, "loadPosts: $response")
+
+            //Json data is in response parametr /variable, it may cause exception while parsing/formating, so do it try catch
+            try {
+                //we have response as JSON Object
+                val jsonObject = JSONObject(response)
+                try {
+                    nextToken = jsonObject.getString("nextPageToken")
+                    Log.d(TAG, "searchPosts: NextPageToken: $nextToken")
+
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Reached end of the page..", Toast.LENGTH_SHORT).show()
+                    Log.d(TAG, "searchPosts: NextPageToken: Reached end of the page..")
                     nextToken = "end"
                 }
                 //get json array data from json object
